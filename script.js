3f0be6d9f3f9560784eba855daf8ed6ddd49bb41
haseebcheema <ihaseebtariq1@gmail.com>
2024-03-14 14:25:53 +0500

    added a circle that moves with cursor

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+// movement of circle with cursor
+window.addEventListener("mousemove", function(dets){
+    var circle = document.querySelector("#circle");
+    circle.style.transform = `translate(${dets.clientX - 6}px, ${dets.clientY - 6}px)`
+})
