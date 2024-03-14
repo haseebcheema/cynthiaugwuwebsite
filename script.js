6cd1479f6f63ffa23f066c2f9530f0181e4492f8
haseebcheema <ihaseebtariq1@gmail.com>
2024-03-14 15:07:15 +0500

    added gsap animatons on hero

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ window.addEventListener("mousemove", function(dets){
     var circle = document.querySelector("#circle");
     circle.style.transform = `translate(${dets.clientX - 6}px, ${dets.clientY - 6}px)`
 })
+
+// gsap animations on hero
+var tl = gsap.timeline()
+tl.from("#nav", {
+    y: -10,
+    opacity: 0,
+    duration: 1
+})
+
+tl.to(".boundingelem", {
+    y: 0,
+    duration: 1,
+    stagger: 0.2
+})
